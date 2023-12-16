a8a1a3d663bd05b0af9b4679bf19430dc93fe833
IgnCan <igcandia2017@udec.cl>
2023-12-16 17:52:45 -0300

    Implementacion Reserva

--- Paneles/PanelCompra.java
+++ Paneles/PanelCompra.java
@@ -77,9 +77,45 @@ public class PanelCompra extends JPanel implements PanelChangeListener{
         add(backButton);
 
 
-
+        add(new Reservador(subLista));
         revalidate();
         repaint();
+    }
+
+    /**
+     * Este boton finaliza la reserva de los pasajes
+     */
+    public class Reservador extends JButton{
+        public Reservador(ArrayList<Object> subLista){
+            setText("Reservar");
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    for (Object elemento : subLista) {
+                        if(elemento instanceof Asientos){
+                            Asientos bot = (Asientos) elemento;
+                            if (bot.getBackground() == Color.GREEN) {
+                                bot.Desactivacion();
+                                //NO BORRAR HASTA PRUEBA EN CARDLAYOUT
+                                //bot.setEnabled(false);
+                                //bot.setBackground(Color.RED);
+                                //PrecioTotal=PrecioTotal+precioPorBoleto;
+                                //System.out.println(PrecioTotal);
+                            }
+                        }else if(elemento instanceof String){
+                            System.out.println(elemento);
+                        }
+                    }
+//                    vent.recibirLista(asientos);
+//
+//                    System.out.println(listaPrincipal().get(0).get(0) + "dfhsjadgh");
+                    //System.out.println("El precio total de la compra es: " + PrecioTotal);
+                    //PrecioTotal=0;
+                }
+            });
+
+        }
     }
 
     @Override
